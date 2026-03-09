494373445d757ca855b67d1c045ba0efc2579562
Rohan Kumar <kumarrohan0738@gmail.com>
2026-03-10 02:41:19 +0530

    fix nav center

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ const MobileShell = ({ setIsDrawerOpen }) => {
             padding: 6px 16px;
             border-radius: 10px;
             transition: color .15s;
-            min-width: 85px;
+            flex: 1;
           }
           .rm-mob-btn svg {
             display: block;
