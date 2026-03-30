ff6e3be9b15c3f61dcc6e8053ec0f92d7cd8ddea
Rohan Kumar <kumarrohan0738@gmail.com>
2026-03-30 16:36:27 +0530

    style(rui): strictly normalize footer payment logos to identical bounding box specs with optical height tuning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -150,10 +150,16 @@ const Footer = () => {
             © {new Date().getFullYear()} RoadMate Technologies Pvt Ltd. All rights reserved.
           </p>
           <div style={{ display: "flex", gap: "24px", alignItems: "center" }}>
-            <img src="/payment/visa.svg" alt="Visa" style={{ height: "24px" }} />
-            <img src="/payment/mastercard.svg" alt="Mastercard" style={{ height: "34px" }} />
-            <img src="/payment/rupay.svg" alt="RuPay" style={{ height: "26px" }} />
-            <img src="/payment/upi.svg" alt="UPI" style={{ height: "26px" }} />
+            {[
+              { src: "/payment/visa.svg", alt: "Visa", h: "16px" },
+              { src: "/payment/mastercard.svg", alt: "Mastercard", h: "28px" },
+              { src: "/payment/rupay.svg", alt: "RuPay", h: "22px" },
+              { src: "/payment/upi.svg", alt: "UPI", h: "21px" },
+            ].map((card) => (
+              <div key={card.alt} style={{ width: "75px", height: "30px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <img src={card.src} alt={card.alt} style={{ maxHeight: card.h, maxWidth: "100%", width: "auto", objectFit: "contain", filter: card.alt === 'Visa' ? 'brightness(0) invert(1)' : 'none' }} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
