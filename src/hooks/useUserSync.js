7f82d8a511b13b8395a891cbe36cd4b83e0b12c8
Rohan Kumar <kumarrohan0738@gmail.com>
2026-04-10 01:18:35 +0530

    Remove bio field from user profile template

--- src/hooks/useUserSync.js
+++ src/hooks/useUserSync.js
@@ -33,7 +33,6 @@ export const useUserSync = () => {
                 city: "",
                 pincode: "",
                 address: "",
-                bio: "",
                 avatar: user.photoURL || ""
               },
               
