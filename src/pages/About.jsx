560096f04513054433c585b28bb4b2845ff5bb01
Rohan Kumar <kumarrohan0738@gmail.com>
2026-04-12 02:48:47 +0530

    style: align About page content with site standard 1250px ribbon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -154,7 +154,7 @@ const About = () => {
              </div>
 
              {/* OUR STORY / MISSION */}
-             <div style={{ padding: "40px 40px 80px", maxWidth: "1200px", margin: "0 auto", textAlign: "left" }}>
+             <div style={{ padding: "40px 40px 80px", maxWidth: "1250px", margin: "0 auto", textAlign: "left" }}>
                  {/* ABOUT US */}
                  <div className="animate-box">
                     <h2 style={{ fontSize: "22px", fontWeight: 800, fontFamily: H, marginBottom: "20px" }}>
